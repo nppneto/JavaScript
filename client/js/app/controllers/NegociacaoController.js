a68a2f36821040c37fb9eb4e92d6fd01460b1b14
Nelson Neto <nelsonp2n@gmail.com>
2018-10-01 21:43:32 -0300

    Resolvendo problema com data - Formatando data

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,17 +1,48 @@
 class NegociacaoController {
 
+
+    constructor() {
+        let $ = document.querySelector.bind(document);
+
+        this._inputQuantidade = $('#quantidade');
+        this._inputData = $('#data');
+        this._inputValor = $('#valor');
+    }
+
     adiciona(event) {
 
         event.preventDefault();
-        
-        let $ = document.querySelector.bind(document);
-        let inputQuantidade = $('#quantidade');
-        let inputData = $('#data');
-        let inputValor = $('#valor');
 
-        console.log(inputQuantidade.value);
-        console.log(inputData.value);
-        console.log(inputValor.value);
+        // Tirando este trecho deste bloco e colocando-o no construtor,
+        // O programa só precisará buscar uma vez as tags no DOM
+        //-----------------------------------------------------
+        // let $ = document.querySelector.bind(document);
+        // let inputQuantidade = $('#quantidade');
+        // let inputData = $('#data');
+        // let inputValor = $('#valor');
+
+        //console.log(typeof(this._inputData.value));
+
+        // 2016-11-12
+        let data = new Date(...
+            this._inputData.value
+                .split('-')
+                .map(function(item, indice) {
+                    return item - indice % 2;
+                })
+            );
+
+        console.log(data);
+
+        // let negociacao = new Negociacao(
+        //     this._inputQuantidade.value,
+        //     this._inputData.value,
+        //     this._inputValor.value
+        // );
+
+        // console.log(negociacao);
+
+        // Adicionar a negociação em uma lista
 
     }
 
