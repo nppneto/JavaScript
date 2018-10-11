3cf33184fbe87978ebecfb5e4b694bd9398e63b3
Nelson Pereira Pinto Neto <nelson.pereira@neadunimes.net>
2018-10-11 10:46:20 -0300

    Botão Importar Negociações - Concluído

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -66,8 +66,13 @@ class NegociacaoController {
     xhr.onreadystatechange = () => {
       if(xhr.readyState === 4) {
         if(xhr.status === 200) {
-          console.log('Obtendo informações do servidor...');
-          console.log(JSON.parse(xhr.responseText));
+          JSON.parse(xhr.responseText)
+              .map(objeto => new Negociacao(new Date(objeto.data), 
+                objeto.quantidade, objeto.valor))
+                .forEach(negociacao => this._negociacoes.adiciona(negociacao)
+          );
+
+          this._mensagem.texto = "Negociações importadas com sucesso!";
         } else {
           console.log(xhr.responseText);
           this._mensagem.texto = 'Não foi possível obter as negociações da semana!';
