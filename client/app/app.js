f9b4780dfd31f9147cf83ad8a980cc848c9ebf21
Nelson Pereira Pinto Neto <nelson.pereira@neadunimes.net>
2018-10-11 11:03:34 -0300

    Limpando Código

--- client/app/app.js
+++ client/app/app.js
@@ -3,9 +3,9 @@ const controller = new NegociacaoController();
 const $ = document.querySelector.bind(document);
 
 // Associa o evento de submissão do formulário à chamada do método "adiciona"
-$(".form")
-  .addEventListener("submit", controller.adiciona.bind(controller));
-$("#botao-apaga")
-  .addEventListener("click", controller.apaga.bind(controller));
-$('#botao-importa')
-  .addEventListener('click', controller.importaNegociacoes.bind(controller));
+$(".form").addEventListener("submit", controller.adiciona.bind(controller));
+$("#botao-apaga").addEventListener("click", controller.apaga.bind(controller));
+$("#botao-importa").addEventListener(
+  "click",
+  controller.importaNegociacoes.bind(controller)
+);
--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -61,22 +61,26 @@ class NegociacaoController {
 
   importaNegociacoes() {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'negociacoes/semana');
+    xhr.open("GET", "negociacoes/semana");
 
     xhr.onreadystatechange = () => {
-      if(xhr.readyState === 4) {
-        if(xhr.status === 200) {
-          JSON.parse(xhr.responseText)
-              .map(objeto => new Negociacao(new Date(objeto.data), 
-                objeto.quantidade, objeto.valor))
-                .forEach(negociacao => this._negociacoes.adiciona(negociacao)
-          );
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        JSON.parse(xhr.responseText)
+          .map(
+            objeto =>
+              new Negociacao(
+                new Date(objeto.data),
+                objeto.quantidade,
+                objeto.valor
+              )
+          )
+          .forEach(negociacao => this._negociacoes.adiciona(negociacao));
 
-          this._mensagem.texto = "Negociações importadas com sucesso!";
-        } else {
-          console.log(xhr.responseText);
-          this._mensagem.texto = 'Não foi possível obter as negociações da semana!';
-        }
+        this._mensagem.texto = "Negociações importadas com sucesso!";
+      } else {
+        console.log(xhr.responseText);
+        this._mensagem.texto =
+          "Não foi possível obter as negociações da semana!";
       }
     };
     xhr.send();
