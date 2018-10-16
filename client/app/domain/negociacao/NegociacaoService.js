6cb01895edbb6802988079fcfa6244eca8487727
Nelson Neto <nelsonp2n@gmail.com>
2018-10-16 09:34:10 -0300

    Retornando negociações da semana retrasada com Promises

--- client/app/domain/negociacao/NegociacaoService.js
+++ client/app/domain/negociacao/NegociacaoService.js
@@ -45,4 +45,24 @@ class NegociacaoService {
 
   }
 
+  obterNegociacoesDaSemanaRetrasada() {
+    return this._http.get("negociacoes/retrasada").then(
+      dados => {
+        const negociacoes = dados.map(
+          objeto =>
+            new Negociacao(
+              new Date(objeto.data),
+              objeto.quantidade,
+              objeto.valor
+            )
+        );
+
+        return negociacoes;
+      },
+      err => {
+        throw new Error("Não foi possível obter as negociações da semana retrasada");
+      }
+    );
+  }
+
 }
