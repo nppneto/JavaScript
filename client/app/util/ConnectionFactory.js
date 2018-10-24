a65c97deec51982609b302f85476db53fa2d1779
Nelson Neto <nelsonp2n@gmail.com>
2018-10-24 15:55:12 -0300

    Abrindo e Fechando conexão com IndexedDB

--- client/app/util/ConnectionFactory.js
+++ client/app/util/ConnectionFactory.js
@@ -1,47 +1,74 @@
-const stores = ['negociacoes'];
+const ConnectionFactory = (() => {
 
-class ConnectionFactory {
+    const stores = ['negociacoes'];
 
-    constructor() {
+    let connection = null;
 
-        throw new Error('Não é possível criar instâncias da classe.');
+    let close = null;
+
+    return class ConnectionFactory {
+
+        constructor() {
+
+            throw new Error('Não é possível criar instâncias da classe.');
+        }
+
+        static getConnection() {
+
+            return new Promise((resolve, reject) => {
+
+                // se a conexão já foi criada, passa para resolve e conecta
+                if(connection) return resolve(connection);
+
+                const openRequest = indexedDB.open('jscangaceiro', 2);
+
+                openRequest.onupgradeneeded = e => {
+
+                    ConnectionFactory._createStores(e.target.result);
+
+                };
+
+                openRequest.onsuccess = e => {
+                    // só será executado na primeira vez em que a conexão for criada
+                    connection = e.target.result;
+
+                    // guardando função original
+                    close = connection.close.bind(connection);
+                    // lançando a mensagem de erro caso o programador tente finalizar a conexão diretamente
+                    connection.close = () => {
+                        throw new Error('Você não pode fechar diretamente a conexão.');
+                    };
+                    // passa o resultado (conexão) para a Promise
+                    resolve(connection);
+                };
+
+                openRequest.onerror = e => {
+                    console.log(e.target.result);
+                    // passa o erro para o reject da Promise
+                    reject(e.target.error.name);
+                };
+            });
+        }
+
+        static _createStores(connection) {
+            
+            stores.forEach(store => {
+
+                if(connection.objectStoreNames.contains(store))
+                    connection.deleteObjectStore(store);
+
+                connection.createObjectStore(store, { autoIncrement: true });
+            });
+        }
+
+        static closeConnection() {
+
+            // se houver conexão...
+            if(connection)
+                // chamando close original
+                close();
+        }
+
     }
 
-    static getConnection() {
-
-        return new Promise((resolve, reject) => {
-
-            const openRequest = indexedDB.open('jscangaceiro', 2);
-
-            openRequest.onupgradeneeded = e => {
-
-                ConnectionFactory._createStores(e.target.result);
-
-            };
-
-            openRequest.onsuccess = e => {
-                // passa o resultado (conexão) para a Promise
-                resolve(e.target.result);
-            };
-
-            openRequest.onerror = e => {
-                // passa o erro para o reject da Promise
-                console.log(e.target.result);
-                reject(e.target.error.name);
-            };
-        });
-    }
-
-    static _createStores(connection) {
-        
-        stores.forEach(store => {
-
-            if(connection.objectStoreNames.contains(store))
-                connection.deleteObjectStore(store);
-
-            connection.createObjectStore(store, { autoIncrement: true });
-        });
-    }
-
-}
-
+})();
