d9219b1d2b7534ee2ddec41ef1461cd44a8456d8
Nelson Neto <nelsonp2n@gmail.com>
2018-10-25 09:39:47 -0300

    Classe NegociacaoDAO criada e Métodos de Insert e Select

---
+++ client/app/domain/negociacao/NegociacaoDAO.js
@@ -0,0 +1,57 @@
+class NegociacaoDAO {
+  constructor(connection) {
+    this._connection = connection;
+    this._store = "negociacoes";
+  }
+
+  adiciona(negociacao) {
+    return new Promise((resolve, reject) => {
+      // lideremos com a inserção de dados aqui
+      const request = this._connection
+        .transaction([this._store], "readwrite")
+        .objectStore(this._store)
+        .add(negociacao);
+
+      request.onsuccess = e => resolve();
+      request.onerror = e => {
+        console.log(e.target.error);
+        reject("Não foi possível salvar a negociação");
+      };
+    });
+  }
+
+  listaTodos() {
+    return new Promise((resolve, reject) => {
+      // lideremos com o cursor aqui
+
+      const negociacoes = [];
+
+      const cursor = this._connection
+        .transaction([this._store], "readwrite")
+        .objectStore(this._store)
+        .openCursor();
+
+      cursor.onsuccess = e => {
+        const atual = e.target.result;
+
+        if (atual) {
+          const negociacao = new Negociacao(
+            atual.value._data,
+            atual.value._quantidade,
+            atual.value._valor
+          );
+
+          negociacoes.push(negociacao);
+          atual.continue();
+        } else {
+          resolve(negociacoes);
+        }
+      };
+
+      cursor.onerror = e => {
+        console.log(e.target.error);
+        reject("Não foi possível listar as negociações.");
+      };
+    });
+  }
+}
