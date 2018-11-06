3c8b7f2df72469158a2bfcc574b30aad702e3075
Nelson Neto <nelsonp2n@gmail.com>
2018-11-06 11:07:38 -0200

    Função de controle - Debounce.js

--- client/app-src/app.js
+++ client/app-src/app.js
@@ -1,4 +1,5 @@
-import { NegociacaoController } from './controllers/NegociacaoController.js';
+import { NegociacaoController } from "./controllers/NegociacaoController.js";
+import { debounce } from "./util/Debounce.js";
 
 const controller = new NegociacaoController();
 const $ = document.querySelector.bind(document);
@@ -6,7 +7,14 @@ const $ = document.querySelector.bind(document);
 // Associa o evento de submissão do formulário à chamada do método "adiciona"
 $(".form").addEventListener("submit", controller.adiciona.bind(controller));
 $("#botao-apaga").addEventListener("click", controller.apaga.bind(controller));
+// $("#botao-importa").addEventListener(
+//   "click",
+//   debounce(() => controller.importaNegociacoes.bind(controller), 1000)
+// );
 $("#botao-importa").addEventListener(
   "click",
-  controller.importaNegociacoes.bind(controller)
+  debounce(() => {
+    console.log("EXECUTOU A OPERAÇÃO DEBOUNCE");
+    controller.importaNegociacoes();
+  }, 1000)
 );
--- client/app-src/util/index.js
+++ client/app-src/util/index.js
@@ -4,3 +4,4 @@ export * from  './DAOFactory.js';
 export * from  './ApplicationException.js';
 export * from  './HttpService.js';
 export * from  './ProxyFactory.js';
+export * from './Debounce.js';
